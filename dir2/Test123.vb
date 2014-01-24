4712fe52496ed4a1eb82e6bbaf1b580e739d0ab6
Ilia Lessev <ilia.lessev@gmail.com>
2014-01-24 15:11:57 +0200

    Update Test123.vb

--- dir2/Test123.vb
+++ dir2/Test123.vb
@@ -1,7 +1,4 @@
-     mobileproxy.za.astgroup.com
-     
-     And a bit mor talkative :)
-     
+
 Start with GID
   Check local repository
     E:\Projects\ComUnity_Delivery20140205\ComUnity_ImgServer_v2.0
@@ -10,6 +7,4 @@ Start with GID
     https://github.com/ilia-lessev/TestRep1
     
   Try to check in
-    Same Repository
-    Create New One
-    
+ 
